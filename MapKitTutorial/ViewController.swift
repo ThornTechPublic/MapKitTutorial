616d4f84b0fd9e9828a7ec00c6dc41593b2a4191
Robert Chen <robert.chen@thorntechnologies.com>
2015-12-23 12:29:54 -0500

    Initial commit

---
+++ MapKitTutorial/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MapKitTutorial
+//
+//  Created by Robert Chen on 12/23/15.
+//  Copyright © 2015 Thorn Technologies. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
