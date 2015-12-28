c0d56e8b83504fd9d1a354781edb83051a79e3a8
Robert Chen <robert.chen@thorntechnologies.com>
2015-12-28 16:57:14 -0500

    Use UISearchController to display search results

---
+++ MapKitTutorial/LocationSearchTable.swift
@@ -0,0 +1,15 @@
+//
+//  LocationSearchTable.swift
+//  MapKitTutorial
+//
+//  Created by Robert Chen on 12/28/15.
+//  Copyright © 2015 Thorn Technologies. All rights reserved.
+//
+
+import UIKit
+class LocationSearchTable : UITableViewController { }
+
+extension LocationSearchTable : UISearchResultsUpdating {
+    func updateSearchResultsForSearchController(searchController: UISearchController) {
+    }
+}
--- MapKitTutorial/ViewController.swift
+++ MapKitTutorial/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 import MapKit
 
 class ViewController : UIViewController {
+    
+    var resultSearchController:UISearchController? = nil
+    
     let locationManager = CLLocationManager()
     @IBOutlet weak var mapView: MKMapView!
     
@@ -19,6 +22,16 @@ class ViewController : UIViewController {
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
         locationManager.requestWhenInUseAuthorization()
         locationManager.requestLocation()
+        let locationSearchTable = storyboard!.instantiateViewControllerWithIdentifier("LocationSearchTable") as! LocationSearchTable
+        resultSearchController = UISearchController(searchResultsController: locationSearchTable)
+        resultSearchController?.searchResultsUpdater = locationSearchTable
+        let searchBar = resultSearchController!.searchBar
+        searchBar.sizeToFit()
+        searchBar.placeholder = "Search for places"
+        navigationItem.titleView = resultSearchController?.searchBar
+        resultSearchController?.hidesNavigationBarDuringPresentation = false
+        resultSearchController?.dimsBackgroundDuringPresentation = true
+        definesPresentationContext = true
     }
 }
 
